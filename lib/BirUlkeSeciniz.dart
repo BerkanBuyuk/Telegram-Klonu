fe50c49631097427ed6996ad9540e5f868a466b9
Berkan Büyük <berkanbyk55@gmail.com>
2021-11-21 01:32:37 +0300

    Kişi Ekle Düzenlendi.

---
+++ lib/BirUlkeSeciniz.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BirUlkeSeciniz extends StatefulWidget {
+  const BirUlkeSeciniz({Key? key}) : super(key: key);
+
+  @override
+  _BirUlkeSecinizState createState() => _BirUlkeSecinizState();
+}
+
+class _BirUlkeSecinizState extends State<BirUlkeSeciniz> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/KisiEkle.dart
+++ lib/KisiEkle.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:telegram_klonu/BirUlkeSeciniz.dart';
 
 class KisiEkle extends StatefulWidget {
   const KisiEkle({Key? key}) : super(key: key);
 
   @override
   _KisiEkleState createState() => _KisiEkleState();
+
 }
 
 class _KisiEkleState extends State<KisiEkle> {
+
+
+  var tfController1 = TextEditingController();
+  var tfController2 = TextEditingController();
+  var tfController3 = TextEditingController()..text = "Turkey";
+  var tfController4 = TextEditingController()..text = '+90';
+  var tfController5 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF71a6d2),
@@ -21,13 +37,119 @@ class _KisiEkleState extends State<KisiEkle> {
           },)
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-              child: Text("data"),
-          ),
-        ],
+
+      body: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    radius: ekranGenisligi/13,
+                    backgroundColor: Color(0xFF71a6d2),
+                  ),
+                  Column(
+                    children: [
+
+                      SizedBox(
+                        width: ekranGenisligi/2,
+                        //height: ekranYuksekligi/10,
+                        child: TextField(
+                          controller: tfController1,
+                          decoration: InputDecoration(
+                            hintText: "Ad (zorunlu)",
+                            hintStyle: TextStyle(
+                              color: Colors.blueGrey,
+                            ),
+                            filled: false,
+                          ),
+                          autofocus: true,
+                        ),
+                      ),
+
+                      SizedBox(
+                        width: ekranGenisligi/2,
+                        //height: ekranYuksekligi/10,
+                        child: TextField(
+                          controller: tfController2,
+                          decoration: InputDecoration(
+                            hintText: "Soyad (isteğe bağlı)",
+                            hintStyle: TextStyle(
+                              color: Colors.blueGrey,
+                            ),
+                            filled: false,
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: tfController3,
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.blueGrey,
+                  ),
+                  filled: false,
+                ),
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => BirUlkeSeciniz()));
+                },
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: ekranGenisligi/8,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: tfController4,
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                          color: Colors.blueGrey,
+                        ),
+                        filled: false,
+                      ),
+                    ),
+                  ),
+
+                  Spacer(),
+
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: ekranGenisligi/1.3,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: tfController5,
+                        decoration: InputDecoration(
+                          hintText: "- - -   - - -   - - - -",
+                          hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey,
+                          ),
+                          filled: false,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
